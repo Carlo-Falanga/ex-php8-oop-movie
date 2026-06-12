54fbc2ada42c41d69be6237910bfbc97085f2a08
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-12 11:54:23 +0200

    Add .gitignore and implement Movie class in index.php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+
+    class Movie {
+        public $title;
+        public $genre;
+        public $year;
+        public $director;
+
+        public function __construct($_title, $_genre, $_year, $_director) {
+            $this -> title = $_title;
+            $this -> genre = $_genre;
+            $this -> year = $_year;
+            $this -> director = $_director;
+        }
+    }
+
+    $movie = new Movie("Batman", "Action", 2022, "Matt Reeves");
+
+    var_dump($movie);
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
