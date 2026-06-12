bd4053de1c0629d5cd3d383c33cf65cbbb2dcc2a
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-12 12:12:29 +0200

    Refactor Movie class formatting for improved readability

--- index.php
+++ index.php
@@ -1,22 +1,37 @@
 <?php
 
-    class Movie {
-        public $title;
-        public $genre;
-        public $year;
-        public $director;
+class Movie
+{
+    public $title;
+    public $genre;
+    public $year;
+    public $director;
 
-        public function __construct($_title, $_genre, $_year, $_director) {
-            $this -> title = $_title;
-            $this -> genre = $_genre;
-            $this -> year = $_year;
-            $this -> director = $_director;
-        }
+    public function __construct($_title, $_genre, $_year, $_director)
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->year = $_year;
+        $this->director = $_director;
     }
 
-    $movie = new Movie("Batman", "Action", 2022, "Matt Reeves");
+    public function setYear($year)
+    {
+        if ($year >= 1900) {
+            $this -> year = $year;
+        } else {
+            echo "Invalid Year";
+        }
+    }
+}
 
-    var_dump($movie);
+$movie = new Movie("Batman", "Action", 2022, "Matt Reeves");
+
+
+$movie -> setYear(2010);
+
+var_dump($movie);
+
 
 ?>
 
@@ -24,12 +39,15 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - OOP - Movie</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
